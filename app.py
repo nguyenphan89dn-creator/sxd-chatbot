a1ec6e47ea87fa8d4cd7512bb017245cedfeaa7c
nguyenphan89dn-creator <nguyenphan89dn@gmail.com>
2026-05-29 17:34:59 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,29 +1,31 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from fastapi import FastAPI
 from fastapi.responses import FileResponse
+from openai import OpenAI
 import os
 
 app = FastAPI()
-@app.get("/web")
-def web():
-    return FileResponse("index.html")
+
 client = OpenAI(
     api_key=os.environ["OPENAI_API_KEY"]
 )
-
-class ChatRequest(BaseModel):
-    question: str
 
 @app.get("/")
 def home():
     return {"status": "running"}
 
+@app.get("/web")
+def web():
+    return FileResponse("index.html")
+
+class ChatRequest(BaseModel):
+    question: str
+
 @app.post("/chat")
 async def chat(req: ChatRequest):
 
     response = client.responses.create(
-        model="gpt-5.4-mini",
+        model="gpt-4.1-mini",
         input=req.question
     )
 
